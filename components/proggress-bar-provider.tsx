be4ab21462cd7fa87803dd609c16a17dcb4f1a64
dersa17 <dersasembiring11@gmail.com>
2025-09-12 19:53:41 +0700

    add session provider

--- components/proggress-bar-provider.tsx
+++ components/proggress-bar-provider.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
-
+import { SessionProvider } from 'next-auth/react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <SessionProvider>
       {children}
-        <ProgressBar
-          height="4px"
-          options={{ showSpinner: false }}
-          shallowRouting
-        />
-    </>
+      <ProgressBar
+        height="4px"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </SessionProvider>
   );
 };
 
